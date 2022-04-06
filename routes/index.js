2cf0152d49806234c7ca4daf5d67c4ea42ff9f97
manuelangel0909 <archivosesup@hotmail.com>
2022-04-06 18:36:21 -0300

    fix const router

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const express = require ('express')
-router= express.Router
+const router= express.Router
 const mainController = require ('../controllers/mainController')
 router.get('/' , mainController.index)
 router.get('/detalle/:menuId' , mainController.detalle)
